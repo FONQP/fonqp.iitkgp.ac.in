bcb8351220019b0332e4244b8edda376b71f1d54
Monica Pradhan <105774048+Moni89ca@users.noreply.github.com>
2026-01-22 15:49:06 +0530

    Remove outdated news item from newsItems array
    Removed outdated news item about paper acceptance.

--- frontend/src/data/News.jsx
+++ frontend/src/data/News.jsx
@@ -1,12 +1,18 @@
 export const newsItems = [
+    
+    {
+        title: "Paper Accepted in APS Physical Review Applied",
+        date: "16th January 2026",
+        news: "Paper titled 'Femtosecond-laser-based true-random-number generator: Entropy as a service' Published."
+    },
+    {
+        title: "Paper Accepted in APS Physical Review A",
+        date: "13th January 2026",
+        news: "Paper titled 'Secure quantum communication via Einstein-Podolsky-Rosen steering between remotely coupled optomechanical systems' Published."
+    },
     {
         title: "Guest Lecture by Prof. Vitaly F.R. Esquerre",
         date: "24th October 2025",
         news: "Lecture on Photonic Devices: Modelling, Optimization and Reconfigurability"
-    },
-    {
-        title: "Paper Accepted in APS Physical Review Applied",
-        date: "9th December 2025",
-        news: "Paper titled 'Femtosecond-laser-based true-random-number generator: Entropy as a service' accepted for publication."
     },
 ];
